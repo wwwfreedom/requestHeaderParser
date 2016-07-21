59805238551009bb4eb08b741c5f7b93161d59f0
Kevin <tig3r86@gmail.com>
2016-07-22 07:16:10 +0930

    add logic for detecting user ip, language and operating system

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const parser = require('ua-parser-js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  // using user agent parser to parse out the details of the user-agent property
+  var operatingSystem = parser(req.headers['user-agent']).os.name
+  // parsing the language of the user
+  var language = req.headers['accept-language'].split(',')[0]
+  res.send({
+    ipaddress: ip,
+    language: language,
+    software: operatingSystem
+  })
 });
 
 module.exports = router;
